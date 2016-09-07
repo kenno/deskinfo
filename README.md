8d459d98ce798c3a2a243b7d41899ff391b58368
Soputtra San <soputtra.san@unsw.edu.au>
2016-09-07 15:34:22 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Deskinfo
+
+Deskinfo is a small utility program used to display some basic information of a Linux computer.
+
+## Why
+We need to identify the installed operating system by its internal name: Desk 8, 9, and 10.
+
+## Installation
+
+```bash
+go get github.com/kenno/deskinfo
+
+```
+
---
+++ main.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+    "bufio"
+    "bytes"
+    "fmt"
+    "flag"
+    "os"
+    "regexp"
+    "syscall"
+)
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func grep(re, filename string) string {
+    regex, err := regexp.Compile(re)
+    check(err)
+
+    fh, err := os.Open(filename)
+    f := bufio.NewReader(fh)
+    check(err)
+    defer fh.Close()
+
+    buf := make([]byte, 1024)
+    for {
+        buf, _, err = f.ReadLine()
+        if err != nil {
+            return string(buf)
+        }
+
+        s := string(buf)
+        if regex.MatchString(s) {
+            //fmt.Printf("%s\n", string(buf))
+            return string(buf)
+        }
+    }
+}
+
+func getuptime() (float64, error) {
+    info := syscall.Sysinfo_t{}
+    if err := syscall.Sysinfo(&info); err != nil {
+        return 0, err
+    }
+    //fmt.Printf("uptime: ", float64(info.Uptime))
+
+    return float64(info.Uptime), nil
+}
+
+func printuptime(fieldName bool) {
+    buf := new(bytes.Buffer)
+    w := bufio.NewWriter(buf)
+
+    u, _ := getuptime()
+    uptime := uint64(u)
+
+    days := uptime / (60 * 60 * 24)
+
+    if days != 0 {
+        s := ""
+        if days > 1 {
+            s = "s"
+        }
+        fmt.Fprintf(w, "%d day%s ", days, s)
+    }
+
+    minutes := uptime / 60
+    hours := minutes / 60
+    hours %= 24
+    minutes %= 60
+
+    fmt.Fprintf(w, "%2d:%02d", hours, minutes)
+
+    w.Flush()
+
+    if fieldName {
+        fmt.Print("Uptime: ")
+    }
+    fmt.Println(buf.String())
+}
+
+func printhostname(fieldName bool) {
+    hostName, err := os.Hostname()
+    check(err)
+
+    if fieldName {
+        fmt.Print("Hostname: ")
+    }
+    fmt.Println(hostName)
+}
+
+func printos(fieldName bool) {
+    // read /etc/lsb-release to get the codename
+    codename := grep("DISTRIB_CODENAME", "/etc/lsb-release")
+
+    deskCodename := ""
+
+    switch codename {
+    case "DISTRIB_CODENAME=xenial":
+        deskCodename = "Desk 10"
+    case "DISTRIB_CODENAME=trusty":
+        deskCodename = "Desk 9"
+    case "DISTRIB_CODENAME=precise":
+        deskCodename = "Desk 8"
+    default:
+        deskCodename = "Unknown"
+    }
+
+    if fieldName {
+        fmt.Print("OS: ")
+    }
+    fmt.Println(deskCodename)
+}
+
+func main() {
+
+    filterPtr := flag.String("filter", "all", "Filter output by field: all, os, host, uptime")
+    fieldNamePtr := flag.Bool("name", true, "Field name: true, false")
+
+    flag.Parse()
+
+    switch *filterPtr {
+    case "os":
+        printos(*fieldNamePtr)
+    case "host":
+        printhostname(*fieldNamePtr)
+    case "uptime":
+        printuptime(*fieldNamePtr)
+    default:
+        printos(*fieldNamePtr)
+        printhostname(*fieldNamePtr)
+        printuptime(*fieldNamePtr)
+    }
+}
